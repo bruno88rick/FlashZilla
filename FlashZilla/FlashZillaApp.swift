23a7e76eda32709657eca6ed67a8f06b2fc402b6
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-12-30 17:05:18 -0300

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Bruno Oliveira on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
