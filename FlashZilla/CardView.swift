7f39132b03a0b494ae36684031224cf07e62673b
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-12-31 12:05:00 -0300

    Acceccibility changes and some bugs solved

--- FlashZilla/CardView.swift
+++ FlashZilla/CardView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    ///SwiftUI provides a specific environment property that tells us when VoiceOver is running, called accessibilityVoiceOverEnabled
+    @Environment(\.accessibilityVoiceOverEnabled) var accessibilityVoiceOverEnabled
+    
     @State private var isShowingAnswer = false
     ///track how far user drag the card
     @State private var offset = CGSize.zero
@@ -21,10 +25,23 @@ struct CardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
-                .fill(.white)
+            ///As for the white fill opacity, this is going to be similar to the opacity() modifier we added previously except we’ll use 1 minus 1/50th of the gesture width rather than 2 minus the gesture width. This creates a really nice effect: we used 2 minus earlier because it meant the card would have to move at least 50 points before fading away, but for the card fill we’re going to use 1 minus so that it starts becoming colored straight away.
+                .fill(
+                    accessibilityDifferentiateWithoutColor
+                    ? .white
+                    : .white
+                        .opacity(1 - Double(abs(offset.width / 50))) //abs method here to exclude the "-" operator if the offset is less than 0
+                )
+            ///we’ll give it a background of the same rounded rectangle except in green or red depending on the gesture movement, then we’ll make the white fill from above fade out as the drag movement gets larger.
+                .background(
+                    accessibilityDifferentiateWithoutColor
+                    ? nil
+                    : RoundedRectangle(cornerRadius: 25)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             
-            VStack {
+            /*VStack {
                 Text(card.prompt)
                     .font(.largeTitle)
                     .foregroundStyle(.black)
@@ -33,9 +50,28 @@ struct CardView: View {
                         .font(.title)
                         .foregroundStyle(.secondary)
                 }
+            } <- Old Cold with accessibility voice over problem */
+            ///we need to help the system to read the answer to the cards as well as the questions. This is possible right now, but only if the user swipes around on the screen – it’s far from obvious. So, to fix this we’re going to detect whether the user has accessibility enabled on their device, and if so automatically toggle between showing the prompt and showing the answer. That is, rather than have the answer appear below the prompt we’ll switch it out and just show the answer, which will cause VoiceOver to read it out immediately. To solve this we can do:
+            VStack {
+                if accessibilityVoiceOverEnabled {
+                    Text(isShowingAnswer ? card.answer : card.prompt)
+                        .font(.largeTitle)
+                        .foregroundStyle(.black)
+                } else {
+                    Text(card.prompt)
+                        .font(.largeTitle)
+                        .foregroundStyle(.black)
+                    
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundStyle(.secondary)
+                    }
+                }
             }
             .padding(20)
             .multilineTextAlignment(.center)
+            ///If you try that out with VoiceOver you’ll hear that it works much better – as soon as the card is double-tapped the answer is read out.
         }
         .frame(width: 450, height: 250)
         ///Tip: A width of 450 is no accident: the smallest iPhones have a landscape width of 480 points, so this means our card will be fully visible on all devices.
@@ -47,8 +83,11 @@ struct CardView: View {
         ///We’re going to take 1/50th of the drag amount, so the card doesn’t fade out too quickly.
         ///We don’t care whether they have moved to the left (negative numbers) or to the right (positive numbers), so we’ll put our value through the abs() function. If this is given a positive number it returns the same number, but if it’s given a negative number it removes the negative sign and returns the same value as a positive number.
         ///We then use this result to subtract from 2. The use of 2 here is intentional, because it allows the card to stay opaque while being dragged just a little. So, if the user hasn’t dragged at all the opacity is 2.0, which is identical to the opacity being 1. If they drag it 50 points left or right, we divide that by 50 to get 1, and subtract that from 2 to get 1, so the opacity is still 1 – the card is still fully opaque. But beyond 50 points we start to fade out the card, until at 100 points left or right the opacity is 0
-        .opacity(2 - Double(offset.width / 50))
+        .opacity(2 - Double(abs(offset.width / 50))) // abs method here to exclude the "-" operator if the offset is less than 0.
         ///we’ve created a property to store the drag amount, and added three modifiers that use the drag amount to change the way the view is rendered. What remains is the most important part: we need to actually attach a DragGesture to our card so that it updates offset as the user drags the card around. Drag gestures have two useful modifiers of their own, letting us attach functions to be triggered when the gesture has changed (called every time they move their finger), and when the gesture has ended (called when they lift their finger). Both of these functions are handed the current gesture state to evaluate. In our case we’ll be reading the translation property to see where the user has dragged to, and we’ll be using that to set our offset property, but you can also read the start location, predicted end location, and more. When it comes to the ended function, we’ll be checking whether the user moved it more than 100 points in either direction so we can prepare to remove the card, but if they haven’t we’ll set offset back to 0.
+        ///###
+        ///Accessibility problemns solve: We don’t say that the cards are buttons that can be tapped, we need to make it clear that our cards are tappable buttons. This is as simple as adding accessibilityAddTraits() with .isButton
+        .accessibilityAddTraits(.isButton)
         .gesture(
             DragGesture()
                 .onChanged { drag in
@@ -69,6 +108,8 @@ struct CardView: View {
         .onTapGesture {
             isShowingAnswer.toggle()
         }
+        ///Right now if you drag an image a little then let go we set its offset back to zero, which causes it to jump back into the center of the screen. If we attach a spring animation to our card, it will slide into the center, which I think is a much clearer indication to our user of what actually happened.
+        .animation(.bouncy, value: offset)
     }
 }
 
--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -10,14 +10,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    @Environment(\.scenePhase) var scenePhase
+    @Environment(\.accessibilityVoiceOverEnabled) var accessibilityVoiceOverEnabled
+    
     @State private var cards = Array<Card>(repeating: .example, count: 10)
+    @State private var timeRemaining = 100
+    @State private var isActive = true
+    ///We have two var for scenePhase here because the environment value tells us whether the app is active or inactive in terms of its visibility, but we’ll also consider the app inactive is the player has gone through their deck of flashcards – it will be active from a scene phase point of view, but we don’t keep the timer ticking.
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
-            Image(.background)
+            //Image(.background)
+            
+            ///our UI is a bit of a mess when used with VoiceOver. If you launch it on a real device with VoiceOver enabled, you’ll find that you can tap on the background image to get “Background, image” read out, which is pointless. To fix the background image problem we should make it use a decorative image so it won’t be read out as part of the accessibility layout
+            Image(decorative: "background")
                 .resizable()
                 .ignoresSafeArea()
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundStyle(.black)
+                        .clipShape(.capsule)
+                }
+                
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         //CardView(card: cards[index])
@@ -31,17 +59,107 @@ struct ContentView: View {
                             }
                         }
                         .stacked(at: index, in: cards.count)
-                        
+                        ///it’s possible to drag cards around when they aren’t at the top. This is confusing for users because they can grab a card they can’t actually see, so this should never be possible. To fix this we’re going to use allowsHitTesting() so that only the last card – the one on top – can be dragged around
+                        .allowsHitTesting(index == cards.count - 1)
+                        ///make small swipes to the right and VoiceOver will move through all the accessibility elements – it reads out the text from all our cards, even the ones that aren’t visible. To fix the cards, we need to use an accessibilityHidden() modifier with a similar condition to the allowsHitTesting() modifier we added a minute ago. In this case, every card that’s at an index less than the top card should be hidden from the accessibility system because there’s really nothing useful it can do with the card:
+                        .accessibilityHidden(index < cards.count - 1)                        
                     }
                 }
+                ///if allowHitTesting is false in the parameter, swiftUI will disable all kind of interactive (click, gestures and more). This will disable interactive if timeRemaing is 0
+                .allowsHitTesting(timeRemaining > 0)
+            }
+            
+            ///we can make those buttons visible when either accessibilityDifferentiateWithoutColor is enabled or when VoiceOver
+            if accessibilityDifferentiateWithoutColor || accessibilityVoiceOverEnabled {
+                VStack {
+                    Spacer()
+                    
+                    /*HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                    } <- old cold with button voice over problem */
+                    ///Third, we need to make it easier for users to mark cards as correct or wrong, because right now our images just don’t cut it. Not only do they stop users from interacting with our app using tap gestures, but they also get read out as their SF Symbols name – “checkmark, circle, image” – rather than anything useful. To fix this we need to replace the images with buttons that actually remove the cards. We don’t actually do anything different if the user was correct or wrong – I need to leave something for your challenges! – but we can at least remove the top card from the deck. At the same time, we’re going to provide an accessibility label and hint so that users get a better idea of what the buttons do.
+                    HStack {
+                        Button {
+                            withAnimation {
+                                removeCard(at: cards.count - 1)
+                            }
+                        } label: {
+                            Image(systemName: "xmark.circle")
+                                .padding()
+                                .background(.black.opacity(0.7))
+                                .clipShape(.circle)
+                        }
+                        .accessibilityLabel("Wrong")
+                        .accessibilityHint("Mark your answer as being incorrect.")
+                        
+                        Spacer()
+                        
+                        Button {
+                            withAnimation {
+                                removeCard(at: cards.count - 1)
+                            }
+                        } label: {
+                            Image(systemName: "checkmark.circle")
+                                .padding()
+                                .background(.black.opacity(0.7))
+                                .clipShape(.circle)
+                        }
+                        .accessibilityLabel("Correct")
+                        .accessibilityHint("Mark your answer as being correct.")
+                    }
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
+        }
+        .onReceive(timer) { time in
+            ///exits immediately is isActive is false and do not decrement the timeRemaining
+            guard isActive else { return }
+            
+            ///That adds a trivial condition to make sure we never stray into negative numbers.
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                ///making sure isActive stays false when returning from the background – we should just update our scene phase code so it explicitly checks for cards:
+                if cards.isEmpty == false {
+                    isActive = true
+                }
+            } else {
+                isActive = false
             }
         }
     }
     
     ///we can now write a method to handle removing a card, then connect it to that closure in CardView.
     func removeCard(at index: Int) {
+        ///Because those buttons remain onscreen (accessibility buttons) even when the last card has been removed, we need to add a guard check to the start of removeCard(at:) to make sure we don’t try to remove a card that doesn’t exist.
+        guard index >= 0 else { return }
+        
         ///This takes an index in our cards array and removes that item
         cards.remove(at: index)
+        
+        ///stop the timer when the final card is removed
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func resetCards() {
+        cards = Array<Card>(repeating: .example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
     
 }
